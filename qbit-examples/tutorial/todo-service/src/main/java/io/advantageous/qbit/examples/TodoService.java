2b2e89e463c55eb2710f8e640f1e2797283bbd5a
Richard Hightower <richardhightower@gmail.com>
2015-01-23 13:35:02 -0800

    #76 fixed tutorial.

--- qbit-examples/tutorial/todo-service/src/main/java/io/advantageous/qbit/examples/TodoService.java
+++ qbit-examples/tutorial/todo-service/src/main/java/io/advantageous/qbit/examples/TodoService.java
@@ -1,22 +1,37 @@
 package io.advantageous.qbit.examples;
 
 
+import io.advantageous.qbit.annotation.RequestMapping;
+import io.advantageous.qbit.annotation.RequestMethod;
+
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by rhightower on 1/19/15.
  */
+
+@RequestMapping("/todo-service")
 public class TodoService {
 
     List<TodoItem> items = new ArrayList<>();
 
 
+    @RequestMapping("/todo/count")
+    public int size() {
 
+        return items.size();
+    }
+
+
+
+    @RequestMapping("/todo/")
     public List<TodoItem> list() {
         return items;
     }
 
+
+    @RequestMapping(value = "/todo", method = RequestMethod.POST)
     public void add(TodoItem todoItem) {
         items.add(todoItem);
     }
