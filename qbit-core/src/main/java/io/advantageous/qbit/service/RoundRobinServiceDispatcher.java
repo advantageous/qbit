e48269325e7a93929a51691f408468bc362e999d
Richard Hightower <richardhightower@gmail.com>
2015-02-18 14:56:20 -0800

    Workers

---
+++ qbit-core/src/main/java/io/advantageous/qbit/service/RoundRobinServiceDispatcher.java
@@ -0,0 +1,99 @@
+/*******************************************************************************
+* Copyright (c) 2015. Rick Hightower, Geoff Chandler
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*  		http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*  QBit - The Microservice lib for Java : JSON, WebSocket, REST. Be The Web!
+*   http://rick-hightower.blogspot.com/2014/12/rise-of-machines-writing-high-speed.html
+*   http://rick-hightower.blogspot.com/2014/12/quick-guide-to-programming-services-in.html
+*   http://rick-hightower.blogspot.com/2015/01/quick-start-qbit-programming.html
+*   http://rick-hightower.blogspot.com/2015/01/high-speed-soa.html
+*   http://rick-hightower.blogspot.com/2015/02/qbit-event-bus.html
+*/
+
+package io.advantageous.qbit.service;
+
+
+import io.advantageous.qbit.message.MethodCall;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * @author  rhightower
+ * on 2/18/15.
+ */
+public class RoundRobinServiceDispatcher implements Consumer<MethodCall<Object>>, ServiceFlushable, Stoppable{
+
+    private List<Service> services = new ArrayList<>();
+
+    private volatile int index = 0;
+
+    private final boolean startServices;
+
+    public RoundRobinServiceDispatcher(boolean startServices) {
+        this.startServices = startServices;
+    }
+
+
+    public RoundRobinServiceDispatcher() {
+        this.startServices = true;
+    }
+
+
+    public RoundRobinServiceDispatcher addService(Service service) {
+        services.add(service);
+        return this;
+    }
+
+    public RoundRobinServiceDispatcher start() {
+
+        services = Collections.unmodifiableList(services);
+
+        if (startServices) {
+            for (Service service : services) {
+                service.start();
+            }
+        }
+        return this;
+    }
+
+    @Override
+    public void accept(MethodCall<Object> methodCall) {
+
+
+        if (index == services.size()) {
+            index=0;
+        }
+        final Service service = services.get(services.size());
+        //LEFT OFF HERE
+        index++;
+    }
+
+    @Override
+    public void flush() {
+        for (Service service : services) {
+            service.flush();
+        }
+    }
+
+    @Override
+    public void stop() {
+
+        for (Service service : services) {
+            service.stop();
+        }
+    }
+}
---
+++ qbit-core/src/main/java/io/advantageous/qbit/service/impl/ServiceImpl.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2015. Rick Hightower, Geoff Chandler
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * QBit - The Microservice lib for Java : JSON, WebSocket, REST. Be The Web!
+ */
+
+package io.advantageous.qbit.service.impl;
+
+import io.advantageous.qbit.message.*;
+import io.advantageous.qbit.queue.*;
+import io.advantageous.qbit.service.*;
+import io.advantageous.qbit.system.QBitSystemManager;
+
+
+public class ServiceImpl extends BaseServiceImpl {
+
+
+    public ServiceImpl(final String rootAddress,
+                       final String serviceAddress,
+                       final Object service,
+                       final QueueBuilder queueBuilder,
+                       final ServiceMethodHandler serviceMethodHandler,
+                       final Queue<Response<Object>> responseQueue,
+                       final boolean async,
+                       final boolean handleCallbacks,
+                       final QBitSystemManager systemManager) {
+        super(rootAddress, serviceAddress, service, queueBuilder, serviceMethodHandler, responseQueue,
+                async, handleCallbacks, systemManager);
+    }
+}
