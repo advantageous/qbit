b3e70a51d526d1f366866c235cccaa9b68e56664
Geoffrey Chandler <geoffc@gmail.com>
2015-03-30 21:05:51 -0700

    refactor to remove boon

--- testing/qbit-perf/copyDist.sh
+++
@@ -1,2 +0,0 @@
-scp ./qbit-perf-client/build/distributions/qbit-perf-client-1.0.0-SNAPSHOT.zip dev@192.168.2.2:/home/dev/perf
-scp ./qbit-perf-server/build/distributions/qbit-perf-server-1.0.0-SNAPSHOT.zip dev@192.168.2.2:/home/dev/perf
