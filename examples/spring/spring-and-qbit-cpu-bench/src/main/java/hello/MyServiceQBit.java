dab49bb4a70299982282cd94b5f46758788c2ed5
Richard Hightower <richardhightower@gmail.com>
2015-06-08 01:31:42 -0700

    qbit perf

--- examples/spring/spring-and-qbit-cpu-bench/src/main/java/hello/MyServiceQBit.java
+++ examples/spring/spring-and-qbit-cpu-bench/src/main/java/hello/MyServiceQBit.java
@@ -1,11 +1,7 @@
 package hello;
 
 
-import io.advantageous.boon.core.Sys;
 import io.advantageous.qbit.annotation.RequestMapping;
-import io.advantageous.qbit.annotation.RequestParam;
-import io.advantageous.qbit.queue.QueueBuilder;
-import io.advantageous.qbit.server.ServiceEndpointServer;
 
 import java.util.Collections;
 import java.util.List;
@@ -20,22 +16,16 @@ import static io.advantageous.qbit.server.EndpointServerBuilder.endpointServerBu
 @RequestMapping("/myservice")
 public class MyServiceQBit {
 
-    final ActualService actualService = new ActualService();
-    int count = 0;
-
-    public static void main(String... args) throws Exception {
-
-
-        final ServiceEndpointServer serviceEndpointServer = endpointServerBuilder()
-                .setPort(6060).build();
-
-        serviceEndpointServer.initServices(new MyServiceQBit());
-        serviceEndpointServer.start();
-
-    }
-
     @RequestMapping("/ping")
     public List<String> ping() {
         return Collections.singletonList("Hello World!");
     }
+
+    public static void main(String... args) throws Exception {
+        endpointServerBuilder()
+                .setPort(6060)
+                .build()
+                .initServices(new MyServiceQBit()).startServer();
+    }
+
 }
