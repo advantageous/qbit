4719d7293b23697a6d1bde81d7afc7a088ae4e11
Richard Hightower <richardhightower@gmail.com>
2015-07-06 02:25:38 -0700

    found an issue with statsd replciator and fixed it

---
+++ qbit/boon/src/test/java/io/advantageous/qbit/service/CallVoidServiceTest.java
@@ -0,0 +1,7 @@
+package io.advantageous.qbit.service;
+
+/**
+ * Created by rhightower on 7/6/15.
+ */
+public class CallVoidServiceTest {
+}
---
+++ qbit/core/src/main/java/io/advantageous/qbit/queue/UnableToEnqueueHandler.java
@@ -0,0 +1,7 @@
+package io.advantageous.qbit.queue;
+
+/**
+ * Created by rhightower on 7/6/15.
+ */
+public class UnableToEnqueueHandler {
+}
