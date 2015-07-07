3dd342871e8f525cb58d1a85618a02b124a99881
Richard Hightower <rhightower@gmail.com>
2015-07-06 23:50:14 -0700

    adding stats and health to end point server builder

--- qbit/cluster/admin/src/main/java/io/advantageous/qbit/metrics/support/LocalStatsCollectorAsync.java
+++ qbit/cluster/admin/src/main/java/io/advantageous/qbit/metrics/support/LocalStatsCollectorAsync.java
@@ -1,12 +1,10 @@
 package io.advantageous.qbit.metrics.support;
 
 import io.advantageous.qbit.metrics.StatReplicator;
-import io.advantageous.qbit.reactive.Callback;
+
 import io.advantageous.qbit.service.stats.StatCollection;
 
-import java.util.Map;
 
 public interface LocalStatsCollectorAsync extends StatReplicator, StatCollection {
 
-    Map<String, Map<String, ?>> collect(Callback<Map<String, Map<String, ?>>> callback);
 }
