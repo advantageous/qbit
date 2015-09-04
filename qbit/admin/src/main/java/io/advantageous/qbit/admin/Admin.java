7c37a87bc9fb4fff36491bcd875b9a2cf02d99b5
Rick Hightower <hightower_r@apple.com>
2015-09-03 22:09:25 -0700

    added support for more endpoints about the OS and VM for debugging.

--- qbit/admin/src/main/java/io/advantageous/qbit/admin/Admin.java
+++ qbit/admin/src/main/java/io/advantageous/qbit/admin/Admin.java
@@ -21,6 +21,7 @@ package io.advantageous.qbit.admin;
 import io.advantageous.qbit.annotation.QueueCallback;
 import io.advantageous.qbit.annotation.QueueCallbackType;
 import io.advantageous.qbit.annotation.RequestMapping;
+import io.advantageous.qbit.annotation.RequestParam;
 import io.advantageous.qbit.meta.ContextMeta;
 import io.advantageous.qbit.meta.builder.ContextMetaBuilder;
 import io.advantageous.qbit.meta.swagger.MetaTransformerFromQbitMetaToSwagger;
@@ -29,8 +30,12 @@ import io.advantageous.qbit.reactive.Callback;
 import io.advantageous.qbit.reactive.Reactor;
 import io.advantageous.qbit.service.health.HealthServiceAsync;
 import io.advantageous.qbit.service.health.NodeHealthStat;
+import io.advantageous.qbit.service.stats.StatsCollector;
 
+import java.lang.management.*;
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 @RequestMapping("/__admin")
 public class Admin {
@@ -40,6 +45,14 @@ public class Admin {
 
     private final ServiceEndpointInfo serviceEndpointInfo;
     private final Reactor reactor;
+
+
+    private final MemoryMXBean memoryMXBean;
+    private final OperatingSystemMXBean operatingSystemMXBean;
+    private final ThreadMXBean threadMXBean;
+    private final RuntimeMXBean runtimeMXBean;
+
+
 
 
     public Admin(final HealthServiceAsync healthService,
@@ -61,6 +74,12 @@ public class Admin {
 
 
         this.healthService = healthService;
+
+        memoryMXBean = ManagementFactory.getMemoryMXBean();
+        operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
+        threadMXBean = ManagementFactory.getThreadMXBean();
+        runtimeMXBean = ManagementFactory.getRuntimeMXBean();
+
     }
 
     @RequestMapping("/meta/")
@@ -82,6 +101,140 @@ public class Admin {
 
         healthService.findAllNodes(callback::accept);
         healthService.clientProxyFlush();
+    }
+
+    @RequestMapping("/system/property/")
+    public Properties getSystemProperties() {
+        return System.getProperties();
+    }
+
+
+    @RequestMapping("/system/property")
+    public String getSystemProperty(@RequestParam("p") final String propertyName) {
+        return System.getProperties().getProperty(propertyName);
+    }
+
+
+
+    @RequestMapping("/env/variable/")
+    public Map<String, String> getEnvironmentVariables() {
+        return System.getenv();
+    }
+
+    @RequestMapping("/env/variable")
+    public String getEnvironmentVariable(@RequestParam("v") final String variableName) {
+        return System.getProperties().getProperty(variableName);
+    }
+
+    @RequestMapping("/available-processors")
+    public int getAvailableProcessors() {
+        return Runtime.getRuntime().availableProcessors();
+    }
+
+
+    @RequestMapping("/memory/free")
+    public long getFreeMemory() {
+        return Runtime.getRuntime().freeMemory();
+    }
+
+
+    @RequestMapping("/memory/total")
+    public long getTotalMemory() {
+        return Runtime.getRuntime().totalMemory();
+    }
+
+
+    @RequestMapping("/memory/max")
+    public long getMaxMemory() {
+        return Runtime.getRuntime().maxMemory();
+    }
+
+    @RequestMapping("/memory/heap/usage")
+    public long getMemoryHeapUsage() {
+
+        return memoryMXBean.getHeapMemoryUsage().getUsed();
+    }
+
+    @RequestMapping("/memory/non-heap/usage")
+    public long getMemoryNonHeapUsage() {
+        return memoryMXBean.getNonHeapMemoryUsage().getUsed();
+    }
+
+
+    @RequestMapping("/thread/count")
+    public int getThreadCount() {
+        return threadMXBean.getThreadCount();
+    }
+
+
+    @RequestMapping("/os/load-average")
+    public double getSystemLoadAverage() {
+        return operatingSystemMXBean.getSystemLoadAverage();
+    }
+
+
+    @RequestMapping("/os/name")
+    public String getOSName() {
+        return operatingSystemMXBean.getName();
+    }
+
+    @RequestMapping("/os/arch")
+    public String getOSArch() {
+        return operatingSystemMXBean.getArch();
+    }
+
+
+    @RequestMapping("/os/version")
+    public String getOSVersion() {
+        return operatingSystemMXBean.getVersion();
+    }
+
+
+    @RequestMapping("/runtime/classpath")
+    public String getClassPath() {
+        return runtimeMXBean.getClassPath();
+    }
+
+
+    @RequestMapping("/runtime/boot-classpath")
+    public String getBootClassPath() {
+        return runtimeMXBean.getBootClassPath();
+    }
+
+
+    @RequestMapping("/runtime/vm-version")
+    public String getVmVersion() {
+        return runtimeMXBean.getVmVersion();
+    }
+
+
+    @RequestMapping("/runtime/vm-vendor")
+    public String getVmVendor() {
+        return runtimeMXBean.getVmVendor();
+    }
+
+
+    @RequestMapping("/runtime/lib-pat")
+    public String getLibPath() {
+        return runtimeMXBean.getLibraryPath();
+    }
+
+
+    @RequestMapping("/runtime/spec-name")
+    public String getSpecName() {
+        return runtimeMXBean.getSpecName();
+    }
+
+
+
+    @RequestMapping("/runtime/spec-version")
+    public String getSpecVersion() {
+        return runtimeMXBean.getSpecVersion();
+    }
+
+    @RequestMapping("/runtime/spec-vendor")
+    public String getSpecVendor() {
+        return runtimeMXBean.getSpecVendor();
     }
 
 
