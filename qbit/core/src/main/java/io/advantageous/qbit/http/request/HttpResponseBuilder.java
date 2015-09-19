4ce4bf33fa09fea7196e625793ffd1c4e0cd9586
Rick Hightower <hightower_r@apple.com>
2015-09-18 17:55:43 -0700

    Documentation improvements

--- qbit/core/src/main/java/io/advantageous/qbit/http/request/HttpResponseBuilder.java
+++ qbit/core/src/main/java/io/advantageous/qbit/http/request/HttpResponseBuilder.java
@@ -5,14 +5,39 @@ import io.advantageous.qbit.http.request.impl.HttpTextResponseImpl;
 import io.advantageous.qbit.util.MultiMap;
 import io.advantageous.qbit.util.MultiMapImpl;
 
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Builds an HTTP Response.
+ */
 public class HttpResponseBuilder {
 
 
+    /**
+     * Response code.
+     */
     private int code;
+
+    /**
+     * Content Type, e.g., "application/json"
+     */
     private String contentType;
+
+
+    /**
+     * Response body.
+     */
     private Object body;
+
+    /**
+     * Response headers.
+     */
     private MultiMap<String, String> headers;
 
+    /**
+     * Builder creator
+     * @return httpResponseBuilder
+     */
     public static HttpResponseBuilder httpResponseBuilder() {
         return new HttpResponseBuilder();
     }
@@ -35,6 +60,31 @@ public class HttpResponseBuilder {
         return this;
     }
 
+
+    /** Sets a JSON body. */
+    public HttpResponseBuilder setJsonBody(final String json) {
+        this.setContentType("application/json");
+        this.setBody(json);
+        return this;
+    }
+
+
+    /** Sets a JSON body with OK (200) http code. */
+    public HttpResponseBuilder setJsonBodyCodeOk(final String json) {
+        this.setCode(200);
+        this.setContentType("application/json");
+        this.setBody(json);
+        return this;
+    }
+
+    /** Sets a JSON body with (500) http code. */
+    public HttpResponseBuilder setJsonBodyError(final String json) {
+        this.setCode(500);
+        this.setContentType("application/json");
+        this.setBody(json);
+        return this;
+    }
+
     public Object getBody() {
         return body;
     }
@@ -51,6 +101,12 @@ public class HttpResponseBuilder {
         return headers;
     }
 
+    /**
+     * Add a header
+     * @param name name
+     * @param value value
+     * @return HttpResponseBuilder
+     */
     public HttpResponseBuilder addHeader(final String name, final String value) {
         getHeaders().put(name, value);
         return this;
@@ -61,11 +117,43 @@ public class HttpResponseBuilder {
         return this;
     }
 
+    /**
+     * Build a response object.
+     * @return HttpResponse
+     */
     public HttpResponse<?> build() {
         if (getBody() instanceof  byte[]) {
             return new HttpBinaryResponseImpl(getCode(), getContentType(), (byte[]) getBody(), headers);
         } else {
             return new HttpTextResponseImpl(getCode(), getContentType(), getBody().toString(), headers);
+        }
+    }
+
+
+    /**
+     * Build a text response object.
+     * @return HttpResponse
+     */
+    public HttpTextResponse buildTextResponse() {
+        if (getBody() instanceof  byte[]) {
+            return new HttpTextResponseImpl(getCode(), getContentType(),
+                    new String((byte[])getBody(), StandardCharsets.UTF_8), headers);
+        } else {
+            return new HttpTextResponseImpl(getCode(), getContentType(), getBody().toString(), headers);
+        }
+    }
+
+
+    /**
+     * Build a binary response object.
+     * @return HttpResponse
+     */
+    public HttpBinaryResponse buildBinaryResponse() {
+        if (getBody() instanceof  byte[]) {
+            return new HttpBinaryResponseImpl(getCode(), getContentType(), (byte[]) getBody(), headers);
+        } else {
+            return new HttpBinaryResponseImpl(getCode(), getContentType(),
+                    getBody().toString().getBytes(StandardCharsets.UTF_8), headers);
         }
     }
 }
