2217fcb74c53fb7d878212d717bfe604641eab1e
Rick Hightower <hightower_r@apple.com>
2015-11-04 19:53:52 -0800

    Adding more examples for JMS queue.

---
+++ examples/standalone/src/main/java/io/advantageous/qbit/example/jms/JmsUtil.java
@@ -0,0 +1,31 @@
+package io.advantageous.qbit.example.jms;
+
+import io.advantageous.qbit.jms.JmsServiceBuilder;
+import io.advantageous.qbit.jms.JmsTextQueue;
+import io.advantageous.qbit.queue.JsonQueue;
+import io.advantageous.qbit.queue.Queue;
+
+public class JmsUtil {
+
+
+    public static Queue<Todo> createQueue() {
+
+        /** Create a new JMS Builder which can emit JmsService objects. */
+        final JmsServiceBuilder jmsBuilder = JmsServiceBuilder
+                .newJmsServiceBuilder().setPort(5000)
+                .setDefaultDestination("todoQueue");
+
+        /** Create a QBit Queue that talks to JMS. */
+        final Queue<String> textQueue = new JmsTextQueue(jmsBuilder);
+
+
+        /**
+         * Queue
+         */
+        final Queue<Todo> queue = new JsonQueue<>(Todo.class, textQueue);
+
+
+        return queue;
+    }
+
+}
--- examples/standalone/src/main/java/io/advantageous/qbit/example/rawmap/HandleMap.java
+++ examples/standalone/src/main/java/io/advantageous/qbit/example/rawmap/HandleMap.java
@@ -4,8 +4,6 @@ import io.advantageous.qbit.QBit;
 import io.advantageous.qbit.admin.ManagedServiceBuilder;
 import io.advantageous.qbit.annotation.RequestMapping;
 import io.advantageous.qbit.annotation.http.PUT;
-import static io.advantageous.qbit.http.request.HttpResponseBuilder.httpResponseBuilder;
-import io.advantageous.qbit.http.request.HttpTextResponse;
 import io.advantageous.qbit.json.JsonMapper;
 import io.advantageous.qbit.reactive.Callback;
 
@@ -17,13 +15,10 @@ public class HandleMap {
     private final JsonMapper jsonMapper = QBit.factory().createJsonMapper();
 
     @PUT("/put")
-    public void putMap(final Callback<HttpTextResponse> httpCallback,
+    public void putMap(final Callback<Map<String,Object>> httpCallback,
                        final Map<String, Object> map) {
 
-        httpCallback.returnThis(
-                httpResponseBuilder()
-                        .setBody(jsonMapper.toJson(map))
-                        .buildTextResponse());
+        httpCallback.returnThis(map);
 
     }
 
