16485097cbf4725b0e5e6fe407a793fde2743730
RichardHightower <richard.m.hightower@gmail.com>
2016-03-24 17:58:26 -0700

    Wrote test app and client to test bridge integration with node

---
+++ qbit/vertx/src/test/java/io/advantageous/qbit/vertx/BridgeToTestNodeJS.java
@@ -0,0 +1,83 @@
+package io.advantageous.qbit.vertx;
+
+import io.advantageous.qbit.service.ServiceBuilder;
+import io.advantageous.qbit.service.ServiceQueue;
+import io.advantageous.qbit.vertx.eventbus.bridge.VertxEventBusBridgeBuilder;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.sockjs.BridgeOptions;
+import io.vertx.ext.web.handler.sockjs.PermittedOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
+
+public class BridgeToTestNodeJS extends AbstractVerticle {
+
+
+    public static class TestService {
+
+        public boolean test(final String newValue) {
+
+            System.out.println("HERE::" + newValue);
+            return true;
+        }
+    }
+
+    @Override
+    public void start() throws Exception {
+
+        /* test service */
+        final TestService testService = new TestService();
+
+        /* address */
+        final String address = "testservice";
+
+        /* service builder */
+        final ServiceBuilder serviceBuilder = ServiceBuilder.serviceBuilder();
+        serviceBuilder.setServiceObject(testService);
+        final ServiceQueue serviceQueue = serviceBuilder.build();
+
+
+
+
+        /* vertx event bus bridge to qbit. */
+        final VertxEventBusBridgeBuilder vertxEventBusBridgeBuilder = VertxEventBusBridgeBuilder
+                .vertxEventBusBridgeBuilder()
+                .setVertx(vertx);
+
+        vertxEventBusBridgeBuilder.addBridgeAddress(address);
+
+        final Router router = Router.router(vertx);
+
+
+        router.route("/health/").handler(routingContext -> routingContext.response().end("\"ok\""));
+
+
+            /* Configure bridge at this HTTP/WebSocket URI. */
+        router.route("/eventbus/*").handler(SockJSHandler.create(vertx).bridge(
+                new BridgeOptions()
+                        .addInboundPermitted(new PermittedOptions().setAddress(address))
+                        .addOutboundPermitted(new PermittedOptions().setAddress(address))
+        ));
+
+
+        vertxEventBusBridgeBuilder.setServiceQueue(serviceQueue);
+        serviceQueue.start(); //startall not supported yet for bridge.
+        vertxEventBusBridgeBuilder.build();
+
+
+        vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+
+        System.out.println("Bound to 8080");
+    }
+
+    public static void main(String... args) throws Exception {
+
+
+        Vertx vertx = Vertx.vertx();
+
+        vertx.deployVerticle(new BridgeToTestNodeJS());
+
+    }
+}
---
+++ qbit/vertx/src/test/javascript/testClient.js
@@ -0,0 +1,48 @@
+const EventBus = require("vertx3-eventbus-client");
+
+
+function connect(url) {
+  console.log(`Connecting to vertx: ${url}`);
+  return new Promise((resolve, reject) => {
+    try {
+      const eventBus = new EventBus(url);
+      eventBus.onopen = () => {
+        console.log(`established connection to vertx on ${url}`);
+        resolve(eventBus);
+      };
+      eventBus.onclose = () => {
+        console.log(`connection closed on ${url}`);
+      };
+      eventBus.onerror = (error) => {
+        console.log("there was an error: ", error);
+        reject(error);
+      }
+    } catch (e) {
+      console.log(`exception encountered connecting to ${url}`);
+      reject(e);
+    }
+  });
+}
+
+connect('http://localhost:8080/eventbus/').then((eventBus) => {
+
+  console.log("HERE.....")
+
+  eventBus.send("testservice", '{"method":"test", "args":["hello"]}', (error, result)=>{
+
+    if (error) {
+      console.log("error message ", error);
+    } else {
+      console.log("result message ", result);
+    }
+
+
+  });
+
+}).catch((error) => {
+  console.log(error)
+});
+
+setInterval(function () {
+  console.log('Waiting...');
+}, 3000);
