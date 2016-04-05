ad0e0779c95577b0802aaf411de129cc7d2c4a6e
RichardHightower <richard.m.hightower@gmail.com>
2016-04-05 15:20:16 -0700

    test case to test bug fix.

---
+++ qbit/vertx/src/test/java/io/advantageous/qbit/vertx/bugs/Bug660.java
@@ -0,0 +1,46 @@
+package io.advantageous.qbit.vertx.bugs;
+
+import io.advantageous.boon.core.Sys;
+import io.advantageous.qbit.http.client.HttpClient;
+import io.advantageous.qbit.http.client.HttpClientBuilder;
+import io.advantageous.qbit.http.request.HttpRequestBuilder;
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static io.advantageous.boon.core.IO.puts;
+import static org.junit.Assert.assertEquals;
+
+public class Bug660 {
+
+    @Test
+    public void test() throws Exception {
+        final HttpClient httpClient = HttpClientBuilder.httpClientBuilder()
+                .setHost("localhost")
+                .setPort(9999)
+                .buildAndStart();
+
+        AtomicInteger codeRef = new AtomicInteger();
+
+
+        httpClient.sendHttpRequest(HttpRequestBuilder
+                .httpRequestBuilder()
+                .setJsonBodyForPost("\"hi mob\"")
+                .setResponse((code, contentType, body) -> {
+                    puts(code, contentType, body);
+                    codeRef.set(code);
+                })
+                .build());
+
+        for (int index = 0; index < 100; index++) {
+            if (codeRef.get()!=0) {
+                break;
+            }
+            Sys.sleep(1);
+        }
+
+        assertEquals(503, codeRef.get());
+
+    }
+
+}
